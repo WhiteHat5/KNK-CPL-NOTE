53239ca2fe5d139c685ea1a844b034301de27b88
WhiteHat5 <85943581+WhiteHat5@users.noreply.github.com>
2021-06-27 22:49:23 +0900

    Create reverseTwoDigit.c

---
+++ reverseTwoDigit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int begin, end;
+    printf("Enter a two-digit number: ");
+    scanf("%1d%1d",&begin, &end);
+    printf("The reversal is: %d%d",end,begin);
+    
+    return 0;
+}
