ebbdb4e6386f767e6848aca766952b44f3d97e44
WhiteHat5 <85943581+WhiteHat5@users.noreply.github.com>
2021-07-03 23:21:18 +0900

    Create GCDWithEuclid.c

---
+++ GCDWithEuclid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int m , n , rem;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &m, &n);
+    
+    while (n!=0){
+        rem = m%n;
+        m = n;
+        n = rem;
+    }
+    
+    printf("Greatest common divisor: %d\n",m);
+    return 0;
+}
