ecf438f40c0045c4bf49006611d12d3957e534fa
WhiteHat5 <85943581+WhiteHat5@users.noreply.github.com>
2021-07-10 19:18:30 +0900

    Create typeSizeTest.c

---
+++ typeSizeTest.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+     printf("%lu, %lu, %lu, %lu, %lu, %lu\n", 
+           (unsigned long) sizeof(int), (unsigned long) sizeof(short),
+           (unsigned long) sizeof(long), (unsigned long) sizeof(float),
+           (unsigned long) sizeof(double), (unsigned long) sizeof(long double));
+
+    return 0;
+}
