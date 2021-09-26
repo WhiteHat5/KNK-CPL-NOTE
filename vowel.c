33db8ec7c69e4c51b98b4ec44a0d6bfa282d0145
WhiteHat5 <null>
2021-09-26 14:49:13 +0000

    add string control

--- vowel.c
+++ vowel.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int compute_vowel_count(const char *sentence);
+void read_line(char *sentence);
 
 int main()
 {
-    char temp;
-    int cnt=0;
-    
+    char s[50];
     printf("Enter a sentence: ");
-    
-    while(1)    {
-        scanf("%c",&temp);
-        switch(temp){
-            case 'a': case 'A': 
-            case 'e': case 'E': 
-            case 'i': case 'I': 
-            case 'o': case 'O':
-            case 'u': case 'U':
-                cnt++;
-                break;
-            default:
-                break;
-        }
-        if(temp== '\n')
-            break;
-    }
-    
-    printf("Your sentence contains %d vowels\n", cnt);
+    read_line(s);
+    printf("Your sentence contains %d vowels\n", compute_vowel_count(s));
 
     return 0;
+}
+
+int compute_vowel_count(const char *sentence)
+{
+	int cnt = 0;
+	while(*sentence) {
+		switch(*sentence++) {
+			case 'a': case 'A': 
+			case 'e': case 'E': 
+			case 'i': case 'I': 
+			case 'o': case 'O':
+			case 'u': case 'U':
+				cnt++;
+				break;
+			default:
+				break;
+		}
+	}
+	
+	return cnt;
+}
+
+void read_line(char *sentence)
+{
+	while((*sentence++=getchar()) != '\n')
+	;
+	*sentence = '\0';
+	
 }
